061157fcb6f2f4ec37d2f94c0f5bf7ff6852bb56
Sergey <blackakula@blackakula-note.(none)>
2010-08-26 18:07:31 +0300

    htaccess done

--- public/index.php
+++ public/index.php
@@ -1,1 +1,1 @@
-<?php echo htmlspecialchars($_GET['path'])."<br />\n"; echo htmlspecialchars($_SERVER['QUERY_STRING'])."<br />\n"; ?>
+<?php echo htmlspecialchars($_GET['__path__'])."<br />\n"; echo htmlspecialchars($_SERVER['QUERY_STRING'])."<br />\n"; ?>
