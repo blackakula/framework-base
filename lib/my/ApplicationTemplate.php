6ca0df64124c59a23cf1b9b40dc8a4f4c46d87f3
Sergey <blackakula@blackakula-note.(none)>
2010-09-15 09:16:59 +0300

    Prepared for haml

--- lib/my/ApplicationTemplate.php
+++ lib/my/ApplicationTemplate.php
@@ -1,4 +1,15 @@
 <?php
   class ApplicationTemplate extends Template {
+    public function __construct($controller,$action,$params) {
+      parent::__construct($controller,$action,$params);
+      $this->extentions = array('php','haml');
+    }
+
+    public function render($filename, $ext = 'php') {
+      if ($ext !== 'haml') return parent::render($filename,$ext);
+      $h = new Haml(); //(file_get_contents($filename));
+      echo $h->renderFile(null,$filename,array(),true);
+//      echo $h->render(file_get_contents($filename));
+    }
   }
 ?>
